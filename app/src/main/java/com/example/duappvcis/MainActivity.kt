a3e66ba6124551a1117d78ca1dd2c892d54bcffd
PriyanshuYadav08 <priyanshu81104@gmail.com>
2025-07-02 12:31:05 +0530

    More UI updated.

--- app/src/main/java/com/example/duappvcis/MainActivity.kt
+++ app/src/main/java/com/example/duappvcis/MainActivity.kt
@@ -14,6 +14,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
           val du_web_btn = findViewById<Button>(R.id.du_web_btn)
+          val about_du_btn = findViewById<Button>(R.id.about_du_btn)
+          val du_lib_btn = findViewById<Button>(R.id.du_lib_btn)
+          val du_amenities_btn = findViewById<Button>(R.id.du_amenities_btn)
+          val campus_map = findViewById<Button>(R.id.campus_map)
+          val syllabi_btn = findViewById<Button>(R.id.syllabi_btn)
 //        val locateColleges = findViewById<Button>(R.id.locateColleges)
 //        val aboutDU = findViewById<Button>(R.id.aboutDU)
 //        val coursesOffered = findViewById<Button>(R.id.coursesOffered)
@@ -22,6 +27,21 @@ class MainActivity : AppCompatActivity() {
 
           du_web_btn.setOnClickListener {
               openInBrowser("https://www.du.ac.in/")
+          }
+          about_du_btn.setOnClickListener {
+              openInBrowser("https://www.du.ac.in/uploads/duflyer2023/DU-CENTENARY_Brochure_1.pdf")
+          }
+          du_lib_btn.setOnClickListener {
+              openInBrowser("https://www.du.ac.in/index.php?page=du-library-system")
+          }
+          du_amenities_btn.setOnClickListener {
+              openInBrowser("https://www.du.ac.in/index.php?page=Amenities-2")
+          }
+          campus_map.setOnClickListener {
+              openInBrowser("https://www.du.ac.in/uploads/du_map.jpg")
+          }
+          syllabi_btn.setOnClickListener {
+              openInBrowser("https://www.du.ac.in/index.php?page=nep-ugcf-2022-syllabi")
           }
 //        locateColleges.setOnClickListener {
 //            openInBrowser("https://www.du.ac.in/index.php?page=colleges-at-du")
